fb6af716733ac2abeb0a982f0ad02f3d257895ae
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-07-09 14:19:44 -0500

    Attempted to write challenge before result

---
+++ pre-javascript/javascript.js
@@ -0,0 +1,26 @@
+
+if (fname == coachesName ) {
+    console.log ("It's a coach")
+}
+
+if ( greeting == "bonjour" ) {
+    console.log ("comment ca va")
+}
+
+if ( score >= 10 ) {
+    console.log ("you won the game")
+}
+
+if (age > 65) {
+    console.log ("time to retire")
+}
+
+if (hasCovid == true) {
+    console.log("time to isolate")
+} else {
+    console.log("keep social distancing")
+}
+
+if (age > 18 && isBritish == true)  {
+    console.log ("You can vote in the next election")
+}
