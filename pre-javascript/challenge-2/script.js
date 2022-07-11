9314a2ce44d892799ac62e5b93e93414b4e6e954
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-07-11 12:38:51 -0500

    COmpleted greatest number challenge

--- pre-javascript/challenge-2/script.js
+++ pre-javascript/challenge-2/script.js
@@ -1,7 +1,7 @@
 function positiveOrNegative(event) {
 
     let numb= event.target.value;
-    
+
     if (!numb) {
         return;
     }
@@ -17,3 +17,25 @@ function positiveOrNegative(event) {
     alert ("This number is " + result)
 
 }
+//This function is comented out until I can figure out how to get more than one input from a user
+//function greatestNumber(event) {
+
+    //let num1 = event.target.value;
+    
+const num1= 5;
+const num2= 77;
+const num3= 76;
+
+if (num1 >= num2 && num1 >= num3) {
+    console.log(num1 + " is the greatest number");
+} else if (num2>= num1 && num2 >= num3) {
+    console.log(num2 + " is the greatest number");
+} else {
+    console.log(num3 + " is the greatest number")
+}
+
+
+
+
+
+//}
