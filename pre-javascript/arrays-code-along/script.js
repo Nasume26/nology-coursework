7a2b3a5b04f90b9e3bc8acef6bb654855d8b6d18
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-07-12 13:52:33 -0500

    Added for loop with comments

--- pre-javascript/arrays-code-along/script.js
+++ pre-javascript/arrays-code-along/script.js
@@ -4,3 +4,11 @@ console.log(coaches[1])
 
 const pets = ["Fido", "Rusty", "Jack"];
 console.log(pets[2])
+
+
+//let i = 0 is the variable declaration for the loop
+//i < 5 are the circumstances, while i is equal to less than 5 continue looping
+//i++ adds 1 to i everytime a single loop is completed.
+for (let i = 0; i < 5; i++) {
+    console.log("The index on this loop was " + i);
+}
