42fb073e6c3e05d97bdcbcbe7b0ae384b761d2aa
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-07-12 14:49:34 -0500

    Added largest of three numbers function

--- pre-javascript/Final-Challenge/script.js
+++ pre-javascript/Final-Challenge/script.js
@@ -20,3 +20,20 @@ function convertDaysToAge (ammountOfDays) {
 
 console.log (convertDaysToAge(3650));
 console.log (convertDaysToAge(6570));
+
+//Write a function that takes three numbers and returns the largest of the three numbers.
+
+function getLargestNumber(num1,num2,num3) {
+    if (num1 >= num2 && num1 >= num3) {
+        return num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        return num2;
+    } else {
+        return num3;
+    }
+}
+const numberMessage= " is the largest number.";
+
+console.log(getLargestNumber(2,1,4) + numberMessage)
+console.log(getLargestNumber(6,2,3) + numberMessage)
+
