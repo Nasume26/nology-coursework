e540708b864ff458e887d3ddfc839dcb3d0e399e
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-07-12 12:48:25 -0500

    Added snickers function

--- pre-javascript/code-along-1/functions.js
+++ pre-javascript/code-along-1/functions.js
@@ -37,3 +37,18 @@ function getDescription(children,partnerName,location,title) {
 getDescription(1,"Jane","Geogria","Developer")
 getDescription(2,"Betty","Texas","Farmer")
 getDescription(4,"Dave","Florida","Janitor")
+
+
+
+function calculateSupply(age,ammountDay) {
+    let maxAge= 80;
+    let ammountRemaining= maxAge - age;
+    let ammountPerYear = ammountDay * 365
+    let ammountResult = ammountRemaining * ammountPerYear;
+
+    console.log("You will need " + ammountResult + " Snickers to last you until the ripe old age of " + maxAge);
+}
+
+calculateSupply(23,3)
+calculateSupply(43,2)
+calculateSupply(62,1)
