fe310ea7f4ef1b05ac817f90bd06345718fa9a97
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-07-12 12:02:19 -0500

    Perimeter attempt complete

---
+++ pre-javascript/code-along-1/functions.js
@@ -0,0 +1,20 @@
+        
+        function findPerimeter(width, height) {
+            let ammountPerimeter = width + width + height + height;
+            console.log ("The perimeter is " + ammountPerimeter)
+
+}
+
+findPerimeter(10, 22)
+
+function findArea() {
+
+
+}
+
+
+function findCircumference () {
+
+
+
+}
