375021968274ebcf9744f6613943bd99291b7392
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-07-12 12:11:45 -0500

    Added Area and circumference functions

--- pre-javascript/code-along-1/functions.js
+++ pre-javascript/code-along-1/functions.js
@@ -1,20 +1,27 @@
         
         function findPerimeter(width, height) {
             let ammountPerimeter = width + width + height + height;
-            console.log ("The perimeter is " + ammountPerimeter)
+            console.log ("The perimeter is " + ammountPerimeter);
 
 }
 
 findPerimeter(10, 22)
 
-function findArea() {
+function findArea(radius) {
+    let area;
+    area= radius * radius * 3.14;
+    console.log ("The area is " + area);
+
+}
+
+findArea(4)
+
+function findCircumference (diameter) {
+    let circumference;
+    circumference= 3.14 * diameter;
+    console.log ("The Circumference is " + circumference);
 
 
 }
 
-
-function findCircumference () {
-
-
-
-}
+findCircumference (20)
